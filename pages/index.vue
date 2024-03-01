d65f0d2df41f98dfb4350733672419dfafac1c0e
Wladi Veras <wladinart@gmail.com>
2024-03-01 08:50:58 -0300

    chore: remove svg and update button color

--- pages/index.vue
+++ pages/index.vue
@@ -98,7 +98,7 @@ const page = reactive({
         scale: true,
         button: {
           label: 'Assinar',
-          color: 'green'
+          color: 'pink'
         },
         features: [
           '3 produtos de beleza de marcas premium',
@@ -271,15 +271,7 @@ useSeoMeta({
               title="YouTube video player" frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowfullscreen></iframe>
-            <svg class="absolute inset-0 h-full w-full stroke-gray-950/10 dark:stroke-white/10" fill="none">
-              <defs>
-                <pattern id="pattern-5c1e4f0e-62d5-498b-8ff0-cf77bb448c8e" x="0" y="0" width="10" height="10"
-                  patternUnits="userSpaceOnUse">
-                  <path d="M-3 13 15-5M-5 5l18-18M-1 21 17 3" />
-                </pattern>
-              </defs>
-              <rect stroke="none" fill="url(#pattern-5c1e4f0e-62d5-498b-8ff0-cf77bb448c8e)" width="100%" height="100%" />
-            </svg>
+
           </div>
         </div>
       </div>
