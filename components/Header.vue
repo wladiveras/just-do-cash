3060b746d6260ac39c90b437079fd4b2a461874d
Wladi Veras <wladinart@gmail.com>
2024-03-03 05:53:34 -0300

    feat: add translation to header

--- components/Header.vue
+++ components/Header.vue
@@ -1,26 +1,28 @@
 <script setup lang="ts">
+const { t } = useI18n()
+
 const nuxtApp = useNuxtApp()
 const { activeHeadings, updateHeadings } = useScrollspy()
 
 const name = ref('Beleza Natural')
 
 const links = computed(() => [{
-  label: 'Serviços',
+  label: t('header.services'),
   to: '#features',
   icon: 'i-heroicons-cube-transparent',
   active: activeHeadings.value.includes('features') && !activeHeadings.value.includes('pricing')
 }, {
-  label: 'Planos',
+  label: t('header.plans'),
   to: '#pricing',
   icon: 'i-heroicons-credit-card',
   active: activeHeadings.value.includes('pricing') && !activeHeadings.value.includes('testimonials')
 }, {
-  label: 'Recomendações',
+  label: t('header.recommendations'),
   to: '#testimonials',
   icon: 'i-heroicons-academic-cap',
   active: activeHeadings.value.includes('testimonials')
 }, {
-  label: 'FAQ',
+  label: t('header.faq'),
   to: '#faq',
   icon: 'i-heroicons-question-mark-circle',
   active: activeHeadings.value.includes('faq')
@@ -70,21 +72,20 @@ const handleLogout = async () => {
     <template #right>
       <ToggleLocation size="sm" />
       <UColorModeButton size="sm" />
-      {{ $t('welcome') }}
+
       <div v-if="!user">
-        <UButton @click="handleLogin" :label="loading ? 'Carregando ...' : 'Entrar'" color="white" :disabled="loading"
-          variant="ghost" trailing-icon="i-heroicons-arrow-right-20-solid" class="hidden lg:flex" />
+        <UButton @click="handleLogin" :label="loading ? $t('header.sigin_loading') : $t('header.sigin')" color="white"
+          :disabled="loading" variant="ghost" trailing-icon="i-heroicons-arrow-right-20-solid" class="hidden lg:flex" />
       </div>
 
       <div v-else class="flex">
-        <UButton to="dashboard" label="Dashboard" color="white" :disabled="loading" variant="ghost"
+        <UButton to="dashboard" color="white" :disabled="loading" variant="ghost"
           trailing-icon="mdi:view-dashboard-edit" class="hidden lg:flex" />
 
-        <UButton @click="handleLogout" :label="loading ? 'Carregando ...' : 'Sair'" color="white" :disabled="loading"
-          variant="ghost" trailing-icon="i-heroicons-arrow-left-20-solid" class="hidden lg:flex" />
+        <UButton @click="handleLogout" :label="loading ? $t('header.signout_loading') : $t('header.signout')"
+          color="white" :disabled="loading" variant="ghost" trailing-icon="i-heroicons-arrow-left-20-solid"
+          class="hidden lg:flex" />
       </div>
-
-
 
     </template>
 
@@ -93,12 +94,14 @@ const handleLogout = async () => {
 
       <UDivider class="my-6" />
 
-      <UButton @click="handleLogin" v-if="!user" label="Entrar" :disabled="loading" color="white" block class="mb-3">
-        {{ loading ? 'Carregando ...' : 'Entrar' }}
+      <UButton @click="handleLogin" v-if="!user" :label="t('header.sigin')" :disabled="loading" color="white" block
+        class="mb-3">
+        {{ loading ? t('header.sigin_loading') : t('header.sigin') }}
       </UButton>
 
-      <UButton @click="handleLogout" v-else label="Sair" color="white" :disabled="loading" block class="mb-3">
-        {{ loading ? 'Carregando ...' : 'Sair' }}
+      <UButton @click="handleLogout" v-else :label="t('header.signout')" color="white" :disabled="loading" block
+        class="mb-3">
+        {{ loading ? t('header.signout_loading') : t('header.signout') }}
       </UButton>
     </template>
   </UHeader>
