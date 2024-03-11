93483c265ddb9fe19f24164331b8dc8e0309ab6f
wladi Veras <wladinart@gmail.com>
2024-03-11 17:47:20 -0300

    Update SCSS import in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -68,7 +68,7 @@ export default defineNuxtConfig({
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@use "@/assets/scss/main.scss" as *;',
+          additionalData: '@use "@/assets/scss/_vars.scss" as *;',
         },
       },
     },
